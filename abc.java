0bdc52a8323087121a09136256bdda2eb2e269e0
sankar <sankar@email>
2017-08-30 07:54:15 +0530

    as

---
+++ abc.java
@@ -0,0 +1,11 @@
+class abc
+{
+
+public static void main(String[] args)
+{
+int a=60,b=50,c=300;
+c=a+b+c;
+System.out.println("The sum of Three values of C"+c);
+System.out.println("HELLO JAVA");
+}
+}
